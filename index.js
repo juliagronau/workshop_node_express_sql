6f11e50b96f7985ee263b93b772e9e5d40b03513
Julia <77528734+juliagronau@users.noreply.github.com>
2021-03-29 13:26:14 +0200

    enable crud operations and add docu to /

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { getUsers } from "./controllers/users.js";
+import {
+  getUsers,
+  getUserById,
+  createNewUser,
+  editUser,
+  deleteUserById,
+} from "./controllers/users.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -9,6 +15,23 @@ const port = process.env.PORT || 3000;
 // Enable body parsing for JSON
 app.use(express.json());
 
-app.route("/users").get(getUsers);
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>This is a user API</h1><br><h2>Those are the endpoints available:<h2/><p>To retrieve all users: /users</p><p>To retrieve a single user: /users/:id</p>"
+  );
+});
+
+app.route("/users").get(getUsers).post(createNewUser);
+app.route("/users/:id").get(getUserById).put(editUser).delete(deleteUserById);
 
 app.listen(port, () => console.log(`Server running in port: ${port}`));
